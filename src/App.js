aaf536fbb9803981c5aab754d7ed548cf95a54ca
KenshinTerashima <kenguitar0818@gmail.com>
2024-02-19 12:56:29 +0900

    各箇所にクラス名を追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
 import {v4 as uuidv4} from 'uuid';
+import './App.css'
 
 
 function App() {
@@ -34,13 +35,21 @@ const handleClear = () => {
 }
 
   return(
-    <>
+    <div className="app">
+
+      <h1>TodoList</h1>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef}  />
+
+    <div className="btn"> 
       <button onClick={handAddTodo}>追加</button>
       <button onClick={handleClear}>削除</button>
-      <div>残りのタスク: {todos.filter((todo) => !todo.completed).length}</div>
-    </>
+    </div>
+
+      <div className="rest">
+        残りのタスク: {todos.filter((todo) => !todo.completed).length}
+      </div>  
+    </div>
   );
 }
     
